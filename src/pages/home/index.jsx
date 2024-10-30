6be4ee822974db3a6c263be299b6f292d8d0f098
Marcos Vinicius <viniciusmarcos136@gmail.com>
2024-10-29 21:24:05 -0300

    Start of creating the home page

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,10 +1,27 @@
-import {Container} from "./style"
-import {Header} from "../../components/header"
+import { Container, Main } from "./style";
+import { Header } from "../../components/header";
+import { Button } from "../../components/button";
+import { FiPlus } from "react-icons/fi";
 
-export function Home(){
-    return(
-        <Container>
-            <Header/>
-        </Container>
-    )
+export function Home() {
+  return (
+    <Container>
+      <Header />
+      <Main>
+        <div className="title-page">
+          <h1>
+            Meus filmes
+            <Button
+              title={
+                <>
+                  <FiPlus />
+                  Adicionar filme
+                </>
+              }
+            />
+          </h1>
+        </div>
+      </Main>
+    </Container>
+  );
 }
