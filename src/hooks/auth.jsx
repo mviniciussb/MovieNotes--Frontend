0f7849263ecbef50bd6fa5f75492cd0cdc3f884e
Marcos Vinicius <108037526+mviniciussb@users.noreply.github.com>
2025-03-05 20:20:23 -0300

    ....

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,10 +1,25 @@
 import { createContext, useContext } from "react";
+import api from "../service/api"
 
 export const AuthContext = createContext({})
 
 function AuthProvider({children}){
+
+    async function signIn({email, password}){
+        try {
+            const response = await api.post("/sessions", { email, password });
+        } catch (error) {
+            if(error.response){
+                alert(error.response.data.message)
+            } else {
+                alert("Não foi possivel entrar.")
+            }
+        }
+    }
+
+
     return (
-        <AuthContext.Provider value={{name: "mv", email: "dsakd"}}>
+        <AuthContext.Provider value={{signIn}}>
             {children}
         </AuthContext.Provider>
     )
--- src/pages/signIn/index.jsx
+++ src/pages/signIn/index.jsx
@@ -7,31 +7,42 @@ import { Button } from "../../components/button";
 import {Link} from "react-router-dom"
 
 import { useAuth } from "../../hooks/auth";
+import { useState } from "react";
 
 export function SignIn() {
 
-  const data = useAuth()
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const {signIn} = useAuth()
+
+  function handleSignIn(){
+    signIn({email, password})
+  }
   
 
   return (
     <Container>
       <Form>
-        
-          <h1>RocketMovies</h1>
-          <p>Aplicação para acompanhar tudo que assistir.</p>
-          <h2>Faça seu login</h2>
-        
+        <h1>RocketMovies</h1>
+        <p>Aplicação para acompanhar tudo que assistir.</p>
+        <h2>Faça seu login</h2>
 
         <div>
-          <Input type="text" icon={FiMail} placeholder="E-mail" />
-          <Input type="password" icon={FiLock} placeholder="Senha" />
+          <Input
+            type="text"
+            icon={FiMail}
+            placeholder="E-mail"
+            onChange={e => setEmail(e.target.value)}
+          />
+          <Input type="password" icon={FiLock} placeholder="Senha" onChange={e => setPassword(e.target.value)}/>
         </div>
 
-        <Button title="Entrar" />
+        <Button title="Entrar" onClick={handleSignIn} />
 
         <Link to="/signup">Criar conta</Link>
       </Form>
-      <Img/>
+      <Img />
     </Container>
   );
 }
