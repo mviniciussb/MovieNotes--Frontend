02eecb54d35eb481a2e932c60d90e4759f65887a
Marcos Vinicius <108037526+mviniciussb@users.noreply.github.com>
2025-03-09 18:46:14 -0300

    Update index.jsx

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -4,7 +4,15 @@ import { Button } from "../../components/button";
 import { Note } from "../../components/note";
 import { FiPlus } from "react-icons/fi";
 
+import { useNavigate } from "react-router-dom";
+
 export function Home() {
+  const navigate = useNavigate();
+
+  function handleNewNote() {
+    navigate("/createmovie");
+  }
+
   return (
     <Container>
       <Header />
@@ -12,7 +20,7 @@ export function Home() {
         <div className="title-page">
           <h2>Meus filmes</h2>
           <Button
-            to="/createmovie"
+            onClick={handleNewNote}
             title={
               <>
                 <FiPlus />
@@ -44,7 +52,7 @@ export function Home() {
               title: "Caverna do Dragão",
               rating: 4,
               description: `Hank, Eric, Diana, Presto, Sheila e Bobby – que, após embarcarem em um passeio de montanha-russa em um parque de diversões, são transportados para um reino mágico repleto de criaturas místicas e perigos. Ao chegarem, eles recebem habilidades e armas mágicas de um guia chamado Mestre dos Magos, que os orienta em sua busca para encontrar o caminho de volta para casa.
-              No mundo mágico, o grupo enfrenta desafios e inimigos, sendo o mais perigoso o Vingador, um vilão que deseja capturar seus poderes mágicos. Ao            longo da jornada, os jovens exploram reinos fantásticos e aprendem lições sobre coragem, amizade e sacrifício, mas nunca encontram uma maneira            definitiva de retornar ao mundo real, deixando o final da série aberto e cheio de mistério.`,
+              No mundo mágico, o grupo enfrenta desafios e inimigos, sendo o mais perigoso o Vingador, um vilão que deseja capturar seus poderes mágicos. Ao longo da jornada, os jovens exploram reinos fantásticos e aprendem lições sobre coragem, amizade e sacrifício, mas nunca encontram uma maneira definitiva de retornar ao mundo real, deixando o final da série aberto e cheio de mistério.`,
               tags: [
                 { id: 1, name: "Mestre dos Magos" },
                 { id: 2, name: "Vingador" },
