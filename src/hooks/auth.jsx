7f6e62fd1fdb931ab79adcb15788533d372c1357
Marcos Vinicius <108037526+mviniciussb@users.noreply.github.com>
2025-03-09 15:17:16 -0300

    Update auth.jsx

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,33 +1,62 @@
-import { createContext, useContext } from "react";
-import api from "../service/api"
+import { createContext, useContext, useState, useEffect } from "react";
+import api from "../service/api";
 
-export const AuthContext = createContext({})
+const AuthContext = createContext({});
 
-function AuthProvider({children}){
+function AuthProvider({ children }) {
+  const [data, setData] = useState({});
 
-    async function signIn({email, password}){
-        try {
-            const response = await api.post("/sessions", { email, password });
-        } catch (error) {
-            if(error.response){
-                alert(error.response.data.message)
-            } else {
-                alert("Não foi possivel entrar.")
-            }
-        }
+  async function signIn({ email, password }) {
+    try {
+      const response = await api.post("/sessions", { email, password });
+
+
+      const { token, user } = response.data;
+
+
+      localStorage.setItem("@Rocketnotes: user", JSON.stringify(user));
+      localStorage.setItem("@Rocketnotes: token", token);
+
+
+      api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+
+
+      setData({ token, user });
+    } catch (error) {
+      if (error.response) {
+        alert(error.response.data.error);
+      } else {
+        alert("Não foi possível entrar.");
+      }
     }
+  }
 
+  function signOut() {
+    localStorage.removeItem("@Rocketnotes: token");
+    localStorage.removeItem("@Rocketnotes: user");
+    setData({});
+  }
 
-    return (
-        <AuthContext.Provider value={{signIn}}>
-            {children}
-        </AuthContext.Provider>
-    )
+  useEffect(() => {
+    const token = localStorage.getItem("@Rocketnotes: token");
+    const user = localStorage.getItem("@Rocketnotes: user");
+
+    if (token && user) {
+      api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+      setData({ token, user: JSON.parse(user) });
+    }
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ signIn, signOut, user: data.user }}>
+      {children}
+    </AuthContext.Provider>
+  );
 }
 
-function useAuth(){
-    const context = useContext(AuthContext)
-    return context
+function useAuth() {
+  const context = useContext(AuthContext);
+  return context;
 }
 
-export {AuthProvider, useAuth}
+export { AuthProvider, useAuth };
