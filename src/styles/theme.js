586eed4ec4a69dfd380258d542da9dfd92d698b5
Marcos Vinicius <108037526+mviniciussb@users.noreply.github.com>
2025-03-16 15:39:48 -0300

    up

---
+++ src/styles/theme.js
@@ -0,0 +1,17 @@
+export default {
+  COLORS: {
+    BACKGROUND_900: "#0D0C0F",
+    BACKGROUND_800: "#1C1B1E",
+    BACKGROUND_700: "#262529",
+    BACKGROUND_600: "rgba(255, 133, 155, 0.05)",
+
+    WHITE: "#F4EDE8",
+    PINK: "#B92B29",
+
+    GRAY_100: "#CAC4CF",
+    GRAY_200: "#948F99",
+    GRAY_300: "#999591",
+  },
+};
+
+
